43065d11276528c414465f60a054000e6286eaa6
Yosr Ghanmi <yosraghanmi23@gmail.com>
2025-11-15 01:23:15 +0100

    added poetry and ci corrected

--- tests/test_route_feu.py
+++ tests/test_route_feu.py
@@ -1,11 +1,11 @@
 from models.feu_rouge import *
 from models.route import *
 def test_arret_au_feu_rouge():
-    route = Route("N",30,longueur=200)
+    route = Route("l",longueur=200)
     feu = FeuRouge(cycle=5)
     route.ajouter_feu_rouge(feu, position=50)
 
-    v = Vehicule(id="V1", position=45, vitesse=10)
+    v = Vehicule(id="Vl", position=45, vitesse=10)
     route.vehicules.append(v)
 
     # Feu rouge, le véhicule doit s'arrêter à < 50
